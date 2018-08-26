9ee63d8aeb7d2c9280f4689899aeb681b78cd903
satyasinha94 <satyasinha94@gmail.com>
2018-08-26 19:08:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,12 +11,6 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (string = string.toUpperCase()) {
-    return "YES INDEED!"
-  }
-  if (string = string.toLowerCase()) {
-    return "I can't hear you!"
-  }
   if (string = "I love you, Grandma.") {
     return "I love you, too."
   }

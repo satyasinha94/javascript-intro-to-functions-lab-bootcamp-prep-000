a418e0311d7884851490cfb91438c572790c95f1
satyasinha94 <satyasinha94@gmail.com>
2018-08-26 19:48:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,10 +14,12 @@ function sayHiToGrandma(string) {
   if (string = string.toLowerCase()) {
     return "I can't hear you!"
   }
-  else (string = string.toUpperCase()) {
+  else 
+    if (string = string.toUpperCase()) {
     return "YES INDEED!"
   }
-  else (string = "I love you, Grandma.") {
+  else 
+    if (string = "I love you, Grandma.") {
     return "I love you, too."
   }
 }

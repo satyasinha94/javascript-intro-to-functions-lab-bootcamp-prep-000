f3deb15a627249d4361728226f48545f601d649a
satyasinha94 <satyasinha94@gmail.com>
2018-08-26 18:33:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+function sayHiToGrandma(string) {
+var uppercase = "HELLO"
+uppercase.toUpperCase() === uppercase
+var lowercase = "hello"
+uppercase.tolowerCase() === lowercase
+var mixedcase = "Hi"
+mixedcase.toUpperCase() === mixedcase
+mixedcase.toLowerCase() === mixedcase
+if (string = uppercase) {
+  return "YES INDEED"
+}
+if (string = lowercase) {
+  return "I can't hear you!"
+}
